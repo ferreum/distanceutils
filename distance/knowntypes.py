395198bcaa851800d52af85f6269d117651f3d29
Daniel K <code.danielk@gmail.com>
2017-07-01 03:08:33 +0200

    cleanup knowntypes

--- distance/knowntypes.py
+++ distance/knowntypes.py
@@ -12,6 +12,13 @@ from .levelinfos import LevelInfos, FTYPE_LEVELINFOS
 from .level import Level
 
 
+PROBER = BytesProber({
+    FTYPE_LEADERBOARD: Leaderboard,
+    FTYPE_LEVELINFOS: LevelInfos,
+})
+
+
+@PROBER.func
 def _detect_other(section):
     if section.ident == SECTION_TYPE:
         if section.filetype.startswith(FTYPE_REPLAY_PREFIX):
@@ -21,15 +28,9 @@ def _detect_other(section):
     return None
 
 
-KNOWN_TYPES = BytesProber(types={
-    FTYPE_LEADERBOARD: Leaderboard,
-    FTYPE_LEVELINFOS: LevelInfos,
-}, funcs=[_detect_other])
-
-
-detect_class = KNOWN_TYPES.detect_class
-parse = KNOWN_TYPES.parse
-parse_maybe_partial = KNOWN_TYPES.parse_maybe_partial
+detect_class = PROBER.detect_class
+parse = PROBER.parse
+parse_maybe_partial = PROBER.parse_maybe_partial
 
 
 # vim:set sw=4 ts=8 sts=4 et sr ft=python fdm=marker tw=0:
