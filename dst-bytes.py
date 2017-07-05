6b51a0e6644c379516faa29da164228cab55a1bd
Daniel K <code.danielk@gmail.com>
2017-07-05 18:26:38 +0200

    dst-bytes.py: use single PrintContext for all output

--- dst-bytes.py
+++ dst-bytes.py
@@ -8,7 +8,7 @@ import sys
 import argparse
 import traceback
 
-from distance.bytes import DstBytes
+from distance.bytes import DstBytes, PrintContext
 from distance.knowntypes import maybe_partial
 
 
@@ -27,21 +27,22 @@ def main():
     if not 'nogroups' in flags:
         flags.append('groups')
 
+    p = PrintContext(file=sys.stdout, flags=flags)
+
     have_error = False
     for fname in args.FILE:
         with open(fname, 'rb') as f:
             try:
                 if len(args.FILE) > 1:
-                    print()
-                    print(f.name)
-                dbytes = DstBytes(f)
-                obj, _, exception = maybe_partial(dbytes)
-                print(f"Type: {type(obj).__name__}")
-                obj.print_data(sys.stdout, flags=flags)
+                    p()
+                    p(f"File: {f.name!r}")
+                obj, _, exception = maybe_partial(DstBytes(f))
+                p(f"Type: {type(obj).__name__}")
+                p.print_data_of(obj)
             except KeyboardInterrupt:
                 raise
-            except:
-                traceback.print_exc()
+            except BaseException as e:
+                p.print_exception(e)
                 have_error = True
     return 1 if have_error else 0
 
