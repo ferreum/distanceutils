a49279b7663a0b27db12e7b227e706e3afaf9064
Daniel K <code.danielk@gmail.com>
2017-07-07 00:27:08 +0200

    add test for Level.print_data

--- test/level.py
+++ test/level.py
@@ -11,16 +11,20 @@ if '../' not in sys.path:
     sys.path.append('../')
 
 from distance.level import Level
-from distance.bytes import DstBytes, UnexpectedEOFError
+from distance.bytes import DstBytes, PrintContext, UnexpectedEOFError
 
 
 class LevelTest(unittest.TestCase):
 
     def test_level(self):
         with open("in/level/test-straightroad.bytes", 'rb') as f:
-            dbytes = DstBytes(f)
-            level = Level(dbytes)
+            level = Level(DstBytes(f))
             self.assertEqual(level.level_name, "Test-straightroad")
+
+    def test_print_data(self):
+        p = PrintContext.for_test()
+        with open("in/level/test-straightroad.bytes", 'rb') as f:
+            p.print_data_of(Level(DstBytes(f)))
 
     def test_truncated(self):
         with open("in/level/test-straightroad_truncated.bytes", 'rb') as f:
