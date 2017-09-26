42baf7873fed64b060933fac8d72e73166319f4f
Daniel K <code.danielk@gmail.com>
2017-09-27 00:31:23 +0200

    searchfrags: refactor matching, search for alternate fragment versions

--- distance_scripts/searchfrags.py
+++ distance_scripts/searchfrags.py
@@ -1,7 +1,6 @@
 """Try to find properties of fragments."""
 
 
-import os
 import sys
 import argparse
 import re
@@ -12,9 +11,12 @@ from distance.levelobjects import (
     LevelObject,
     FRAG_PROBER as LEVEL_FRAG_PROBER,
 )
-from distance.bytes import DstBytes, MAGIC_9
+from distance.bytes import (
+    DstBytes, Section,
+    MAGIC_2, MAGIC_3, MAGIC_9
+)
 from distance.printing import PrintContext
-from distance.prober import BytesProber
+from distance.prober import BytesProber, ProbeError
 from distance.base import Fragment
 
 
@@ -58,54 +60,78 @@ def iter_objects(source, recurse=-1):
             yield from iter_objects(obj.children, recurse=recurse-1)
 
 
-def find_matches(offset, data):
-    matches = []
-    pos = 0
-    buf = BytesIO(data)
-    db = DstBytes(buf)
-    while pos < len(data):
-        db.pos = pos
-        try:
-            s = db.read_str()
-        except Exception:
-            pass
-        else:
-            if s and len(list(STR_EXCLUDE_PATTERN.findall(s))) < len(s) // 3:
-                matches.append(("String", repr(s)))
-        db.pos = pos
-        try:
-            i = db.read_int(8)
-        except Exception:
-            pass
-        else:
-            if i >= offset and i <= offset + len(data):
-                matches.append(("Offset", f"0x{i:08x}"))
-        pos += 1
-    return matches
+class FragmentMatcher(object):
+
+    def __init__(self, real_frag_prober):
+        self.real_frag_prober = real_frag_prober
+
+    def find_matches(self, frag):
+        offset = frag.start_pos
+        sec = frag.start_section
+        data = frag.raw_data
+        matches = []
+
+        if sec.magic in (MAGIC_2, MAGIC_3):
+            ver = sec.version
+            versions = []
+            if ver > 0:
+                versions.extend(range(ver))
+            versions.extend(range(ver + 1, ver + 3))
+            for ver in versions:
+                probe_sec = Section(sec.magic, sec.ident, ver)
+                try:
+                    self.real_frag_prober.probe_section(probe_sec)
+                except ProbeError:
+                    pass
+                else:
+                    matches.append(("Other version", repr(probe_sec)))
+
+        pos = 0
+        buf = BytesIO(data)
+        db = DstBytes(buf)
+        while pos < len(data):
+            db.pos = pos
+            try:
+                s = db.read_str()
+            except Exception:
+                pass
+            else:
+                if s and len(list(STR_EXCLUDE_PATTERN.findall(s))) < len(s) // 3:
+                    matches.append(("String", repr(s)))
+            db.pos = pos
+            try:
+                i = db.read_int(8)
+            except Exception:
+                pass
+            else:
+                if i >= offset and i <= offset + len(data):
+                    matches.append(("Offset", f"0x{i:08x}"))
+            pos += 1
+        return matches
 
 
-def visit_object(obj, p):
-    frags = []
-    for frag in obj.fragments:
-        if isinstance(frag, DetectFragment):
-            matches = find_matches(frag.start_pos, frag.raw_data)
-            if matches:
-                frags.append((frag, matches))
+    def visit_object(self, obj, p):
+        frags = []
+        for frag in obj.fragments:
+            if isinstance(frag, DetectFragment):
+                matches = self.find_matches(frag)
+                if matches:
+                    frags.append((frag, matches))
 
-    if frags:
-        p(f"Object: {obj.type!r}")
-        p(f"Offset: 0x{obj.start_pos:08x}")
-        p(f"Candidates: {len(frags)}")
-        with p.tree_children():
-            for frag, matches in frags:
-                p.tree_next_child()
-                p(f"Section: {frag.start_section}")
-                p(f"Offset: 0x{frag.start_pos:08x}")
-                p(f"Matches: {len(matches)}")
-                with p.tree_children():
-                    for name, text in matches:
-                        p.tree_next_child()
-                        p(f"{name}: {text}")
+        if frags:
+            p(f"Object: {obj.type!r}")
+            p(f"Offset: 0x{obj.start_pos:08x}")
+            p(f"Candidates: {len(frags)}")
+            with p.tree_children():
+                for frag, matches in frags:
+                    p.tree_next_child()
+                    p(f"Section: {frag.start_section}")
+                    p(f"Offset: 0x{frag.start_pos:08x}")
+                    p(f"Matches: {len(matches)}")
+                    with p.tree_children():
+                        for name, text in matches:
+                            p.tree_next_child()
+                            p(f"{name}: {text}")
 
 
 def main():
@@ -126,6 +152,8 @@ def main():
         level_obj_prober=p_level,
     )
 
+    matcher = FragmentMatcher(LEVEL_FRAG_PROBER)
+
     with open(args.IN, 'rb') as in_f:
         content = prober.read(DstBytes(in_f), opts=opts)
         if isinstance(content, Level):
@@ -133,9 +161,9 @@ def main():
         else:
             # CustomObject
             object_source = [content]
-        p = PrintContext(file=sys.stdout, flags=('subobjects'))
+        p = PrintContext(file=sys.stdout, flags=())
         for obj in object_source:
-            visit_object(obj, p)
+            matcher.visit_object(obj, p)
 
     return 0
 
