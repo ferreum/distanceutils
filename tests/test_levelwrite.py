69ca9544c769696fd2cce41df5908e2559907cba
Daniel K <code.danielk@gmail.com>
2017-09-27 19:32:29 +0200

    test: move WriteReadTest to separate common module

--- tests/test_levelwrite.py
+++ tests/test_levelwrite.py
@@ -5,63 +5,20 @@ from distance.level import Level
 from distance.bytes import DstBytes
 
 
-def disable_writes(dbytes):
-    def do_raise(*args, **kwargs):
-        raise AssertionError("attempted to write")
-    dbytes.write_bytes = do_raise
-
-
-def write_read(obj, read_func=None):
-    if read_func is None:
-        read_func = type(obj)
-
-    buf = BytesIO()
-    dbytes = DstBytes(buf)
-
-    obj.write(dbytes)
-    dbytes.pos = 0
-    disable_writes(dbytes)
-    result = read_func(dbytes)
-
-    if result.exception:
-        raise result.exception
-
-    return result, buf
-
-
 class Base(object):
 
-    class WriteReadTest(unittest.TestCase):
+    from tests import common
 
-        exact = True
+    class WriteReadTest(common.WriteReadTest):
 
-        def test_read(self):
-            with open(self.filename, 'rb') as f:
-                res = Level(DstBytes(f))
-
-                self.verify_level(res)
-
-        def test_write_read(self):
-            with open(self.filename, 'rb') as f:
-                dbr = DstBytes(f)
-                orig = Level(dbr)
-                orig_len = orig.layers[-1].end_pos
-
-                res, buf = write_read(orig)
-
-                self.verify_level(res)
-                self.assertEqual(orig_len, len(buf.getbuffer()))
-
-                if self.exact:
-                    f.seek(0)
-                    self.assertEqual(f.read(), buf.getbuffer())
+        read_obj = Level
 
 
 class StraightroadTest(Base.WriteReadTest):
 
     filename = "tests/in/level/test-straightroad.bytes"
 
-    def verify_level(self, level):
+    def verify_obj(self, level):
         self.assertEqual("Test-straightroad", level.level_name)
         self.assertEqual(6, len([o for l in level.layers for o in l.objects]))
 
@@ -70,7 +27,7 @@ class TheVirusBeginsS8Test(Base.WriteReadTest):
 
     filename = "tests/in/level-not-included/v1/the virus begins.bytes"
 
-    def verify_level(self, level):
+    def verify_obj(self, level):
         self.assertEqual("The Virus Begins", level.level_name)
 
 
