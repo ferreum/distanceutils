8d3a62c3c47f0feef44b1318dab97432ae6df3bf
Daniel K <code.danielk@gmail.com>
2017-09-30 17:21:15 +0200

    test: use more implicit DstBytes constructor

--- tests/test_level.py
+++ tests/test_level.py
@@ -1,7 +1,7 @@
 import unittest
 
 from distance.level import Level
-from distance.bytes import DstBytes, UnexpectedEOFError
+from distance.bytes import UnexpectedEOFError
 from distance.printing import PrintContext
 from .common import check_exceptions
 
@@ -9,46 +9,39 @@ from .common import check_exceptions
 class LevelTest(unittest.TestCase):
 
     def test_level(self):
-        with open("tests/in/level/test-straightroad.bytes", 'rb') as f:
-            level = Level(DstBytes(f))
-            self.assertEqual(level.level_name, "Test-straightroad")
+        level = Level("tests/in/level/test-straightroad.bytes")
+        self.assertEqual(level.level_name, "Test-straightroad")
 
     def test_print_data(self):
         p = PrintContext.for_test()
-        with open("tests/in/level/test-straightroad.bytes", 'rb') as f:
-            p.print_data_of(Level(DstBytes(f)))
+        p.print_data_of(Level("tests/in/level/test-straightroad.bytes"))
 
     def test_truncated(self):
-        with open("tests/in/level/test-straightroad_truncated.bytes", 'rb') as f:
-            dbytes = DstBytes(f)
-            level = Level(dbytes)
-            self.assertEqual(level.level_name, "Test-straightroad")
-            results = [level.settings] + list(level.iter_objects())
-            self.assertEqual(len(results), 3)
+        level = Level("tests/in/level/test-straightroad_truncated.bytes")
+        self.assertEqual(level.level_name, "Test-straightroad")
+        results = [level.settings] + list(level.iter_objects())
+        self.assertEqual(len(results), 3)
 
     def test_truncated2(self):
-        with open("tests/in/level/test-straightroad_truncated_2.bytes", 'rb') as f:
-            dbytes = DstBytes(f)
-            level = Level(dbytes)
-            self.assertEqual(level.level_name, "Test-straightroad")
-            gen = level.iter_objects()
-            next(gen)
-            obj = next(gen)
-            self.assertEqual(UnexpectedEOFError, type(obj.exception))
+        level = Level("tests/in/level/test-straightroad_truncated_2.bytes")
+        self.assertEqual(level.level_name, "Test-straightroad")
+        gen = level.iter_objects()
+        next(gen)
+        obj = next(gen)
+        self.assertEqual(UnexpectedEOFError, type(obj.exception))
 
     def test_invalid_str(self):
-        with open("tests/in/level/invalid-groupname.bytes", 'rb') as f:
-            level = Level(DstBytes(f))
-            self.assertEqual(level.level_name, "Test Group")
-            results = list(level.iter_objects())
-            self.assertEqual(len(results), 5)
-            for i, obj in enumerate(results):
-                self.assertIsNotNone(obj, f"i == {i}")
-                self.assertTrue(obj.sane_end_pos, f"i == {i}")
-                if i == 2:
-                    self.assertRaises(UnicodeError, check_exceptions, obj)
-                else:
-                    check_exceptions(obj)
+        level = Level("tests/in/level/invalid-groupname.bytes")
+        self.assertEqual(level.level_name, "Test Group")
+        results = list(level.iter_objects())
+        self.assertEqual(len(results), 5)
+        for i, obj in enumerate(results):
+            self.assertIsNotNone(obj, f"i == {i}")
+            self.assertTrue(obj.sane_end_pos, f"i == {i}")
+            if i == 2:
+                self.assertRaises(UnicodeError, check_exceptions, obj)
+            else:
+                check_exceptions(obj)
 
 
 if __name__ == '__main__':
