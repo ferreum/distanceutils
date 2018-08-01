5aef9c20078b979aa1c02642ec07612d45a6ef6c
Daniel K <code.danielk@gmail.com>
2018-08-01 22:47:54 +0200

    remove --gen-autoload from main script

--- distance/__main__.py
+++ distance/__main__.py
@@ -1,21 +1,8 @@
 """Prints distanceutils version when executed."""
 
 
-import argparse
-
-
-parser = argparse.ArgumentParser()
-parser.add_argument('--gen-autoload', action='store_true')
-
-args = parser.parse_args()
-
-
-if args.gen_autoload:
-    from distance import _core
-    _core.write_autoload_modules()
-else:
-    from . import __version__
-    print(f"distanceutils version {__version__}")
+from . import __version__
+print(f"distanceutils version {__version__}")
 
 
 # vim:set sw=4 ts=8 sts=4 et:
