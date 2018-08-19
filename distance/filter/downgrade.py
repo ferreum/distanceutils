24cb421851ce59df2ec0f5b5485771d18cc8e726
Daniel K <code.danielk@gmail.com>
2018-08-19 17:28:58 +0200

    downgrade: show warnings for discarded values

--- distance/filter/downgrade.py
+++ distance/filter/downgrade.py
@@ -7,14 +7,28 @@ from distance._default_classes import DefaultClasses
 from .base import ObjectFilter
 
 
+class DowngradeInfo(object):
+
+    def __init__(self):
+        self.warnings = {}
+
+    def add_if(self, obj, attr, cond):
+        value = getattr(obj, attr)
+        if cond(value):
+            self.warnings[attr] = value
+        return self
+
+
 class AnimatorDowngrader(object):
 
     _tags_ = ['Animator']
 
     def apply(self, obj, sec, index):
         if sec.version > 10:
-            obj['Animator'].container.version = 10
-            return True
+            frag = obj['Animator']
+            frag.container.version = 10
+            return (DowngradeInfo()
+                    .add_if(frag, 'double_pivot_distance', lambda x: (x or 0.0) > 0.05))
         return False
 
 
@@ -24,8 +38,10 @@ class EventListenerDowngrader(object):
 
     def apply(self, obj, sec, index):
         if sec.version > 1:
-            obj['EventListener'].container.version = 1
-            return True
+            frag = obj['EventListener']
+            frag.container.version = 1
+            return (DowngradeInfo()
+                    .add_if(frag, 'delay', lambda x: (x or 0.0) > 0.05))
         return False
 
 
@@ -45,18 +61,22 @@ class DowngradeFilter(ObjectFilter):
         for d in downgraders:
             for tag in d._tags_:
                 key = DefaultClasses.fragments.get_base_key(tag)
-                by_key[key].append(d)
+                by_key[key].append((tag, d))
         self.downgraders_by_key = dict(by_key)
         self.debug = args.debug
         self.num_downgraded = 0
+        self.warnings = []
 
     def downgrade_fragments(self, obj):
         i = 0
         for sec in obj.sections:
             downgraders = self.downgraders_by_key.get(sec.to_key(noversion=True), ())
             if downgraders:
-                for d in downgraders:
-                    if d.apply(obj, sec, i):
+                for tag, d in downgraders:
+                    info = d.apply(obj, sec, i)
+                    if info:
+                        if info.warnings:
+                            self.warnings.append((tag, info.warnings))
                         self.num_downgraded += 1
             i += 1
         return obj,
@@ -69,5 +89,15 @@ class DowngradeFilter(ObjectFilter):
 
     def print_summary(self, p):
         p(f"Downgraded fragments: {self.num_downgraded}")
+        if self.warnings:
+            p(f"Fragment warnings: {len(self.warnings)}")
+            with p.tree_children(len(self.warnings)):
+                for tag, warnings in self.warnings:
+                    p.tree_next_child()
+                    p(f"Fragment {tag!r} warnings: {len(warnings)}")
+                    with p.tree_children(len(warnings)):
+                        for name, value in warnings.items():
+                            p.tree_next_child()
+                            p(f"{name}: {value!r}")
 
 # vim:set sw=4 ts=8 sts=4 et:
