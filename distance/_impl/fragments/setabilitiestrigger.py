65ef011d3971b1facfa42418b079acd8286b49a8
Daniel K <code.danielk@gmail.com>
2018-10-03 14:08:30 +0200

    print data of SetAbilitiesTrigger fragment

--- distance/_impl/fragments/setabilitiestrigger.py
+++ distance/_impl/fragments/setabilitiestrigger.py
@@ -18,6 +18,8 @@ class SetAbilitiesTriggerFragment(BaseConstructFragment):
     base_container = Section.base(Magic[2], 0xad)
     container_versions = 7
 
+    is_interesting = True
+
     _construct_ = Struct(
         'enable_flying' / Default(Byte, 1),
         'enable_jumping' / Default(Byte, 1),
@@ -39,6 +41,24 @@ class SetAbilitiesTriggerFragment(BaseConstructFragment):
         'play_slow_mo_audio' / Default(Byte, 1),
         'visuals_only' / Default(Byte, 0),
     )
+
+    def _visit_print_data(self, p):
+        yield super()._visit_print_data(p)
+        p(f"Enable flying: {self.enable_flying and 'yes' or 'no'}")
+        p(f"Enable jumping: {self.enable_jumping and 'yes' or 'no'}")
+        p(f"Enable boosting: {self.enable_boosting and 'yes' or 'no'}")
+        p(f"Enable jet rotating: {self.enable_jet_rotating and 'yes' or 'no'}")
+        p(f"Infinite cooldown: {self.infinite_cooldown and 'yes' or 'no'}")
+        p(f"Delay: {self.delay}")
+        p(f"Timer text: {self.timer_text!r}")
+        p(f"Ignore in arcade: {self.ignore_in_arcade and 'yes' or 'no'}")
+        p(f"Use slow mo: {self.use_slow_mo and 'yes' or 'no'}")
+        if self.use_slow_mo:
+            p(f"Delay before slow mo: {self.delay_before_slow_mo}")
+            p(f"Slow mo time scale: {self.slow_mo_time_scale}")
+            p(f"Slow mo duration: {self.slow_mo_duration}")
+            p(f"Glitch duration after: {self.glitch_duration_after}")
+        p(f"Visuals only: {self.visuals_only and 'yes' or 'no'}")
 
 
 # vim:set sw=4 et:
